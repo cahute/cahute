f931deb750d65b6a7cc8be84414a33ac420ffe90
Thomas Touhey <thomas@touhey.fr>
2025-09-07 18:15:52 +0200

    chore: add diagnostics for storage devices on Win32

--- lib/platform/win32/usb/ums.c
+++ lib/platform/win32/usb/ums.c
@@ -52,6 +52,208 @@ close_link(cahute_context *context, cahute_win32_ums_link_cookie *cookie) {
 }
 
 /**
+ * Get the storage device properties for the device.
+ *
+ * @param context
+ * @param cookie
+ * @return Cahute error, or 0 if successful.
+ */
+CAHUTE_LOCAL(int)
+get_storage_device_properties(
+    cahute_context *context,
+    cahute_win32_ums_link_cookie *cookie
+) {
+    STORAGE_PROPERTY_QUERY req;
+    STORAGE_DESCRIPTOR_HEADER header_desc;
+    cahute_u8 *desc_buf = NULL,
+              short_desc_buf[sizeof(STORAGE_DEVICE_DESCRIPTOR) + 128];
+    char const *raw_desc_buf;
+    STORAGE_DEVICE_DESCRIPTOR *desc = NULL;
+    DWORD werr, wret, wcnt;
+    int err = CAHUTE_ERROR_UNKNOWN;
+
+    req.PropertyId = StorageDeviceProperty;
+    req.QueryType = PropertyStandardQuery;
+
+    wret = DeviceIoControl(
+        cookie->handle,
+        IOCTL_STORAGE_QUERY_PROPERTY,
+        &req,
+        sizeof(req),
+        &header_desc,
+        sizeof(header_desc),
+        &wcnt,
+        NULL
+    );
+
+    /* Here, we expect the call to be successful even though the
+     * buffer is technically to small. */
+    if (!wret) {
+        werr = GetLastError();
+
+        if (werr == ERROR_INVALID_CATEGORY)
+            CAHUTE_RETURN_IMPL(
+                context,
+                "Cannot request storage device properties."
+            );
+
+        msg(context,
+            ll_error,
+            "Failed obtaining storage device properties' header:");
+        log_windows_error(context, "DeviceIoControl", werr);
+        goto fail;
+    }
+
+    if (wcnt != sizeof(header_desc)) {
+        msg(context,
+            ll_error,
+            "Size of obtained header (%luo) doesn't match expected (%luo)",
+            wcnt,
+            sizeof(header_desc));
+        goto fail;
+    }
+
+    if (header_desc.Size <= sizeof(*desc)) {
+        msg(context,
+            ll_error,
+            "Size of descriptor (%luo) is less than expected (at least %luo)",
+            header_desc.Size,
+            sizeof(*desc));
+        goto fail;
+    }
+
+    if (header_desc.Size <= sizeof(short_desc_buf)) {
+        desc = (void *)short_desc_buf;
+        raw_desc_buf = (void *)short_desc_buf;
+    } else {
+        desc_buf = malloc(header_desc.Size);
+        if (!desc_buf) {
+            err = CAHUTE_ERROR_ALLOC;
+            goto fail;
+        }
+
+        desc = (void *)desc_buf;
+        raw_desc_buf = (void *)desc_buf;
+    }
+
+    wret = DeviceIoControl(
+        cookie->handle,
+        IOCTL_STORAGE_QUERY_PROPERTY,
+        &req,
+        sizeof(req),
+        desc,
+        header_desc.Size,
+        &wcnt,
+        NULL
+    );
+
+    if (!wret) {
+        werr = GetLastError();
+
+        msg(context, ll_error, "Failed obtaining storage device properties:");
+        log_windows_error(context, "DeviceIoControl", werr);
+        goto fail;
+    }
+
+    msg(context, ll_debug, "Obtained storage device properties:");
+    msg(context, ll_debug, "  Device Type: %d", desc->DeviceType);
+    msg(context,
+        ll_debug,
+        "  Device Type Modifier: %d",
+        desc->DeviceTypeModifier);
+    msg(context,
+        ll_debug,
+        "  Removable Media: %s",
+        desc->RemovableMedia ? "yes" : "no");
+    msg(context,
+        ll_debug,
+        "  Command Queueing: %s",
+        desc->CommandQueueing ? "yes" : "no");
+    if (desc->VendorIdOffset)
+        msg(context,
+            ll_debug,
+            "  Vendor ID: %s",
+            &raw_desc_buf[desc->VendorIdOffset]);
+    if (desc->ProductIdOffset)
+        msg(context,
+            ll_debug,
+            "  Product ID: %s",
+            &raw_desc_buf[desc->ProductIdOffset]);
+    if (desc->ProductRevisionOffset)
+        msg(context,
+            ll_debug,
+            "  Product Revision: %s",
+            &raw_desc_buf[desc->ProductRevisionOffset]);
+    if (desc->SerialNumberOffset)
+        msg(context,
+            ll_debug,
+            "  Serial Number: %s",
+            &raw_desc_buf[desc->SerialNumberOffset]);
+
+    err = CAHUTE_OK;
+
+fail:
+    if (desc_buf)
+        free(desc_buf);
+
+    return err;
+}
+
+/**
+ * Get the storage adapter properties for the device.
+ *
+ * @param context
+ * @param cookie
+ * @return Cahute error, or 0 if successful.
+ */
+CAHUTE_LOCAL(int)
+get_storage_adapter_properties(
+    cahute_context *context,
+    cahute_win32_ums_link_cookie *cookie
+) {
+    STORAGE_PROPERTY_QUERY req;
+    STORAGE_ADAPTER_DESCRIPTOR desc;
+    DWORD wret, wcnt, werr;
+
+    req.PropertyId = StorageAdapterProperty;
+    req.QueryType = PropertyStandardQuery;
+
+    wret = DeviceIoControl(
+        cookie->handle,
+        IOCTL_STORAGE_QUERY_PROPERTY,
+        &req,
+        sizeof(req),
+        &desc,
+        sizeof(desc),
+        &wcnt,
+        NULL
+    );
+
+    if (!wret) {
+        werr = GetLastError();
+
+        if (werr == ERROR_INVALID_CATEGORY)
+            CAHUTE_RETURN_IMPL(
+                context,
+                "Cannot request storage device properties."
+            );
+
+        msg(context, ll_error, "Failed obtaining storage adapter properties:");
+        log_windows_error(context, "DeviceIoControl", werr);
+        return CAHUTE_ERROR_UNKNOWN;
+    }
+
+    msg(context, ll_debug, "Obtained storage adapter properties:");
+    msg(context,
+        ll_debug,
+        "  Maximum Transfer Length: %lu",
+        desc.MaximumTransferLength);
+    msg(context, ll_debug, "  Alignment mask: 0x%08lX", desc.AlignmentMask);
+
+    return CAHUTE_OK;
+}
+
+/**
  * Emit an SCSI request on a Win32 link.
  *
  * @param context
@@ -243,6 +445,19 @@ cahute_open_win32_ums_link(
     }
 
     cookie.handle = handle;
+
+    err = get_storage_device_properties(context, &cookie);
+    if (err && err != CAHUTE_ERROR_IMPL) {
+        close_link(context, &cookie);
+        return err;
+    }
+
+    err = get_storage_adapter_properties(context, &cookie);
+    if (err && err != CAHUTE_ERROR_IMPL) {
+        close_link(context, &cookie);
+        return err;
+    }
+
     return cahute_open_ums_link_from_interface(
         open_params,
         &ums_link_interface,
